e8ced90f66de0f9185375cd544df447c67df8445
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2025-05-08 18:27:47 +0200

    push clean up for pushing to pypi

--- helix/types.py
+++ helix/types.py
@@ -1,7 +1,7 @@
 from numpy import float64 as npfloat64
 from numpy.typing import NDArray
 from enum import Enum
-from typing import Dict, Any, Union, List
+from typing import Dict, Any
 
 GHELIX = "\033[32m[HELIX]\033[0m"
 RHELIX = "\033[31m[HELIX]\033[0m"
@@ -16,6 +16,5 @@ class DataType(Enum):
 
 NP_FVec = NDArray[npfloat64]
 Payload = Dict[str, Any]
-#JSONType = Union[Dict[str, Any], List[Any], str, int, float, bool, None]
 
 # TODO: custom exceptions/errors
