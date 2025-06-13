2d3c65f938f24f78cf6d49ac6b3d445905cffe6e
gahnxd <gahnxd@gmail.com>
2025-06-13 03:32:14 +0000

    fix: instance path

--- examples/rag_demo/instance_demo.py
+++ examples/rag_demo/instance_demo.py
@@ -1,5 +1,10 @@
 import sys
-sys.path.append("../../")
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.dirname(os.path.dirname(script_dir))
+sys.path.insert(0, root_dir)
+
 from helix.instance import Instance
 
 helix_instance = Instance("helixdb-cfg", "6969")
