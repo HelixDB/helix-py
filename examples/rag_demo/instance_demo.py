c761ed90e681145a6041ebda17a0edb4ac3c2672
gahnxd <gahnxd@gmail.com>
2025-06-15 01:00:42 +0000

    better  instance management

--- examples/rag_demo/instance_demo.py
+++ examples/rag_demo/instance_demo.py
@@ -1,13 +1,13 @@
 import sys
 import os
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-root_dir = os.path.dirname(os.path.dirname(script_dir))
-sys.path.insert(0, root_dir)
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+# root_dir = os.path.dirname(os.path.dirname(script_dir))
+# sys.path.insert(0, root_dir)
 
 from helix.instance import Instance
 
-helix_instance = Instance("helixdb-cfg", "6969")
+helix_instance = Instance("helixdb-cfg", "6969", verbose=True)
 print("-" * 70 + '\n')
 
 # Deploy
