c5027cff2976538e866458719e4e1e3702facb13
gahnxd <gahnxd@gmail.com>
2025-06-16 22:13:08 +0000

    feat: cookbook example for client

--- examples/cookbook/instance_demo.py
+++ examples/cookbook/instance_demo.py
@@ -1,10 +1,3 @@
-import sys
-import os
-
-# script_dir = os.path.dirname(os.path.abspath(__file__))
-# root_dir = os.path.dirname(os.path.dirname(script_dir))
-# sys.path.insert(0, root_dir)
-
 from helix.instance import Instance
 
 helix_instance = Instance("helixdb-cfg", "6969", verbose=True)
