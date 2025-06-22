f9c3fae8ed2b829d4f2880dfba95bb1846057322
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2025-06-22 09:43:02 +0200

    removed instance to debug now

--- examples/rag-demo/rag_demo_ml_papers.py
+++ examples/rag-demo/rag_demo_ml_papers.py
@@ -105,3 +105,4 @@ if __name__ == "__main__":
 
     load(db)
     query(db)
+
--- helix/__init__.py
+++ helix/__init__.py
@@ -4,7 +4,6 @@
 from helix.client import Client, Query, init, next, call_tool, schema_resource
 from helix.types import Payload
 from helix.loader import Loader
-from helix.instance import Instance
 
 __version__ = "0.2.13"
 
