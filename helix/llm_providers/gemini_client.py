ad4dcac13b7fafb41399bae6defe5c655f4d69a6
Gahn Wuwong <138665743+Gahnxd@users.noreply.github.com>
2025-08-23 17:33:50 -0700

    implement gemini llm provider

---
+++ helix/llm_providers/gemini_client.py
@@ -0,0 +1,91 @@
+from helix.llm_providers.provider import Provider
+from google import genai
+from google.genai import types
+from fastmcp import Client
+from pydantic import BaseModel
+from enum import Enum
+from typing import List, Dict, Any
+from dotenv import load_dotenv
+import os
+
+class Part(BaseModel):
+    text: Any | None = None
+
+class Role(Enum):
+    user = "user"
+    model = "model"
+
+class Message(BaseModel):
+    role: Role
+    parts: List[Part]
+
+class GeminiProvider(Provider):
+    def __init__(
+        self,
+        api_key: str=None,
+        model: str = "gemini-2.0-flash",
+        temperature: float | None = None,
+        thinking_budget: float = 0,
+        history: bool = False,
+    ):
+        if api_key is None:
+            load_dotenv()
+            api_key = os.getenv("GEMINI_API_KEY")
+            if api_key is None:
+                raise ValueError("API key not provided and GEMINI_API_KEY environment variable not set.")
+        self.client = genai.Client(api_key=api_key)
+        self.model = model
+        self.temperature = temperature
+        self.thinking_budget = thinking_budget
+        self.history = [] if history else None
+        self.mcp_client = None
+
+    def enable_mcps(
+        self,
+        url: str = "http://localhost:8000/mcp/"
+    ) -> bool:
+        self.mcp_client = Client(url)
+        return True
+
+    def generate(
+        self,
+        messages: str | List[Message],
+        response_model: BaseModel | None = None
+    ) -> str | BaseModel:
+        if isinstance(self.history, list):
+            if isinstance(messages, list) and all(isinstance(msg, Message) for msg in messages):
+                messages = self.history + [msg.model_dump(mode="json") for msg in messages]
+                self.history = messages
+            elif isinstance(messages, str):
+                messages = self.history + [Message(role=Role.user, parts=[Part(text=messages)]).model_dump(mode="json")]
+                self.history = messages
+            else:
+                raise ValueError("Invalid message type")
+        args = {
+            "model": self.model,
+            "contents": messages
+        }
+        config_args = {}
+        if self.temperature is not None:
+            config_args["temperature"] = self.temperature
+        config_args["thinking_config"] = types.ThinkingConfig(thinking_budget=self.thinking_budget)
+        if response_model is not None:
+            config_args["response_mime_type"] = "application/json"
+            config_args["response_schema"] = response_model
+        if self.mcp_client is not None:
+            with self.mcp_client:
+                config_args["tools"] = [self.mcp_client.session]
+                config = types.GenerateContentConfig(**config_args)
+                args["config"] = config
+                response = self.client.models.generate_content(**args)
+        else:
+            config = types.GenerateContentConfig(**config_args)
+            args["config"] = config
+            response = self.client.models.generate_content(**args)
+        if response_model is not None:
+            result = response_model.model_validate(response.parsed)
+        else:
+            result = response.text
+        if isinstance(self.history, list):
+            self.history.append(Message(role=Role.model, parts=[Part(text=result)]).model_dump(mode="json"))
+        return result
