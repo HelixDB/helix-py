23a203b1f2d411bc374bad0004e7cd19d6c200a6
Gahn Wuwong <138665743+Gahnxd@users.noreply.github.com>
2025-09-13 11:33:52 -0700

    update mcp app

--- apps/mcp_server.py
+++ apps/mcp_server.py
@@ -1,7 +1,5 @@
-# $ uv venv && source .venv/bin/activate
-# $ uv add <all the packages in pyproject.toml or from errors you get>
-# then for claude-desktop add this to ~/Library/Application Support/Claude/claude_desktop_config.json
-#   adjusting paths of course
+# For claude-desktop add this to ~/Library/Application Support/Claude/claude_desktop_config.json
+#   adjusting the directory to where this file is located
 """
 {
   "mcpServers": {
@@ -11,6 +9,8 @@
         "--directory",
         "/Users/ln/dev/helix-py/apps/mcp_server",
         "run",
+        "--python", "3.11",
+        "--with", "helix-py",
         "mcp_server.py"
       ]
     }
@@ -20,13 +20,20 @@
 
 from helix.client import Client
 from helix.mcp import MCPServer
+from helix.embedding import OpenAIEmbedder
 
 # Create a Helix client
 client = Client(local=True, port=6969)
+
+# Create an embedder (optional, needs API key)
+embedder = OpenAIEmbedder()
 
 # Create an MCP server
 mcp_server = MCPServer("helix-mcp", client)
 
 if __name__ == "__main__":
+  # Run the MCP server on stdio transport
+  mcp_server.run(transport="stdio")
+
   # Run the MCP server on localhost port 8000 with streamable-http transport
-  mcp_server.run(transport="streamable-http", host="127.0.0.1", port=8000)
+  # mcp_server.run(transport="streamable-http", host="127.0.0.1", port=8000)
